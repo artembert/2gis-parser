dd6c8024b55363a4f7798abc45324e1887e3d897
Artem Beresnev <artembert@gmail.com>
2020-08-09 12:25:26 +0300

    Adding stores to a file instead of overwriting the file

--- src/data-provider/data-provider-json.ts
+++ src/data-provider/data-provider-json.ts
@@ -7,7 +7,7 @@ export class DataProviderJson implements DataProviderAbstract {
   public async saveMultipleStores(stores: Store[]): Promise<void> {
     const filePath = DistPaths.KALININGRAD;
     try {
-      await promises.writeFile(filePath, JSON.stringify(stores, undefined, 2), "utf8");
+      await promises.appendFile(filePath, JSON.stringify(stores, undefined, 2), "utf8");
       console.log(`Multiple stores saved to ${filePath}`);
     } catch (e) {
       console.error(`Failed to save multiple stores to ${filePath}`, e);
